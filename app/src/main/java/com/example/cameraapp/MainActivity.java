1b23a0b96b8111fd1cca0fa9809ce8107114bffb
WojciechBalcer02 <wojciechbalcer02@gmail.com>
2022-11-23 18:31:49 +0100

    private void startCameraX

--- app/src/main/java/com/example/cameraapp/MainActivity.java
+++ app/src/main/java/com/example/cameraapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.cameraapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
 import android.view.View;
@@ -10,11 +14,13 @@ import android.widget.Button;
 import com.google.common.util.concurrent.ListenableFuture;
 
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
 
     Button btnTakePhoto;
+    PreviewView previewView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +30,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnTakePhoto.setOnClickListener(this);
 
+
         cameraProviderFuture = ProcessCameraProvider.getInstance(this);
         cameraProviderFuture.addListener(() ->{
             try {
                 ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                startCameraX(cameraProvider);
             }catch (ExecutionException e){
                 e.printStackTrace();
             }catch (InterruptedException e){
                 e.printStackTrace();
             }
         }, getExecutor());
+    }
+
+    private Executor getExecutor() {
+        return ContextCompat.getMainExecutor(this);
+    }
+
+    private void startCameraX(ProcessCameraProvider cameraProvider) {
+        cameraProvider.unbindAll();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        Preview preview = new Preview.Builder().build();
+
+        preview.setSurfaceProvider();
     }
 
 
