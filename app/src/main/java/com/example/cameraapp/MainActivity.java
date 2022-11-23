70604c22467d5b7c7663b37b5eb34062b76b0f75
WojciechBalcer02 <wojciechbalcer02@gmail.com>
2022-11-23 18:11:24 +0100

    layout done

--- app/src/main/java/com/example/cameraapp/MainActivity.java
+++ app/src/main/java/com/example/cameraapp/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.cameraapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.lifecycle.ProcessCameraProvider;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+
+    Button btnTakePhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnTakePhoto = findViewById(R.id.buttonPhoto);
+
+        btnTakePhoto.setOnClickListener(this);
+
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(() ->{
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+            }catch (ExecutionException e){
+                e.printStackTrace();
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }, getExecutor());
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.buttonPhoto:
+                break;
+        }
     }
 }
